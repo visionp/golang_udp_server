8a58a93e8ee91f62c9809398daa6f122e83c64a0
visionp <visionp1986@gmail.com>
2019-10-22 13:36:39 +0300

    refactoring

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"udp/realmetric"
 	"udp/server"
 )
 
@@ -22,31 +21,12 @@ func (h handlerPing) Handle(req server.Request, client *server.Client) server.Pa
 }
 
 func main() {
-	handler := realmetric.Start()
 	m := make(map[string]server.HandlerInterface)
 	handlersCollection := &server.HandlersCollection{Handlers: m}
 
 	handlersCollection.Add("handshake", handlerPing{})
 	handlersCollection.Add("ping", handlerPing{})
-	handlersCollection.Add("track", realMetricHandler{handler})
-	handlersCollection.Add("undefined", realMetricHandler{handler})
 
 	var serverUdp = server.Server{Handlers: handlersCollection}
 	serverUdp.Start(":3030")
-}
-
-type realMetricHandler struct {
-	realMetricTrack func(body []byte) (int64, error)
-}
-
-func (h realMetricHandler) Handle(req server.Request, client *server.Client) server.Payload {
-	payload := make(server.Payload)
-
-	payload["_timing"], payload["error"] = h.realMetricTrack(req.Payload)
-
-	payload["action"] = "track"
-	payload["token"] = client.GetToken()
-	payload["count_requests"] = client.GetCountRequestsAsString()
-
-	return payload
 }
